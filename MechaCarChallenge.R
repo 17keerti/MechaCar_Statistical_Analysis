18bcdd5f41563021537fce2f1672f8cd471167ee
Keerti Chaudhary <17keerti@gmail.com>
2022-09-26 16:09:17 -0700

    Perform T-Tests on Suspension Coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -21,3 +21,12 @@ total_summary
 # create summary table by grouping each manufacturing lot
 lot_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
 lot_summary
+
+#Perform t-test on suspension  coils for all Lots
+t.test(suspension_coil_table$PSI, mu=1500)
+# t-test for Lot 1
+t.test(subset(suspension_coil_table,Manufacturing_Lot=="Lot1")$PSI, mu=1500)
+# t-test for Lot 2
+t.test(subset(suspension_coil_table,Manufacturing_Lot=="Lot2")$PSI, mu=1500)
+# t-test for Lot 3
+t.test(subset(suspension_coil_table,Manufacturing_Lot=="Lot3")$PSI, mu=1500)
